1ddc11da1d9d75c2c28374fbe9f5d7bddd2f529f
Derrick Koo <derrick.koo@automattic.com>
2023-04-19 12:00:51 -0600

    add platform to all build scripts

--- build-image-74.sh
+++ build-image-74.sh
@@ -6,7 +6,8 @@ then
     APACHE_USER="$USE_CUSTOM_APACHE_USER"
 fi
 
-docker build -t newspack-dev-74 \
+docker build --platform linux/amd64 \
+    -t newspack-dev-74 \
     --build-arg PHP_VERSION=7.4 \
     --build-arg COMPOSER_VERSION=2.1.14 \
     --build-arg NODE_VERSION=16.14.2 \
