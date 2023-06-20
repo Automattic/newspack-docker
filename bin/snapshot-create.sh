fd95474033373ef34141f0974f0e6781a3e0adc1
Derrick Koo <derrick.koo@automattic.com>
2023-06-20 16:50:02 -0600

    fix: allow-root when creating a snapshot

--- bin/snapshot-create.sh
+++ bin/snapshot-create.sh
@@ -5,7 +5,7 @@ mkdir -p $FOLDER
 
 echo Creating snapshot $1
 echo "dumping database..."
-wp db export $FOLDER/db.sql
+wp db export $FOLDER/db.sql --allow-root
 echo "Copying uploads..."
 mkdir -p $FOLDER/uploads
 cp -r /var/www/html/wp-content/uploads/* $FOLDER/uploads
