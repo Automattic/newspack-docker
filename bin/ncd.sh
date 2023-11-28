e4ff4e1a0b520f30067161d79773f0f0af7588c1
Leo Germani <leogermani@automattic.com>
2023-11-28 14:59:14 -0300

    make it work with MacOS

--- bin/ncd.sh
+++ bin/ncd.sh
@@ -1,34 +1,39 @@
 ncd() {
 
+	# If no argument is provided, go to the root of the newspack-docker folder:
 	if [ -z "$1" ]
 	then
-		echo "You need to inform the name of the directory you want to cd into"
+		cd $NEWSPACK_DOCKER_ROOT
 
 	fi
 
-	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+	# Newspack Manager html:
+	if [[ "$1" == "manager" ]]; then
+		cd "$NEWSPACK_DOCKER_ROOT/manager-html"
+		return
+	fi
 
 	# The complete name of a repo:
-	if [[ -d "$SCRIPT_DIR/../repos/$1" ]]; then
-		cd "$SCRIPT_DIR/../repos/$1"
+	if [[ -d "$NEWSPACK_DOCKER_ROOT/repos/$1" ]]; then
+		cd "$NEWSPACK_DOCKER_ROOT/repos/$1"
 		return
 	fi
 
 	# The name of a repo without the newspack- prefix:
-	if [[ -d "$SCRIPT_DIR/../repos/newspack-$1" ]]; then
-		cd "$SCRIPT_DIR/../repos/newspack-$1"
+	if [[ -d "$NEWSPACK_DOCKER_ROOT/repos/newspack-$1" ]]; then
+		cd "$NEWSPACK_DOCKER_ROOT/repos/newspack-$1"
 		return
 	fi
 
 	# An additional site:
-	if [[ -d "$SCRIPT_DIR/../additional-sites-html/$1" ]]; then
-		cd "$SCRIPT_DIR/../additional-sites-html/$1"
+	if [[ -d "$NEWSPACK_DOCKER_ROOT/additional-sites-html/$1" ]]; then
+		cd "$NEWSPACK_DOCKER_ROOT/additional-sites-html/$1"
 		return
 	fi
 
 	# A plugin in the main site:
-	if [[ -d "$SCRIPT_DIR/../html/wp-content/plugins/$1" ]]; then
-		cd "$SCRIPT_DIR/../html/wp-content/plugins/$1"
+	if [[ -d "$NEWSPACK_DOCKER_ROOT/html/wp-content/plugins/$1" ]]; then
+		cd "$NEWSPACK_DOCKER_ROOT/html/wp-content/plugins/$1"
 		return
 	fi
 
